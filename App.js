bf4946afd044cd3005a4f504edc5c9d6f282500f
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-09-08 01:26:16 -0300

    Troca do View pelo SafeAreaView para preservar a área do StatusBar no IOS
    Troca do View pelo SafeAreaView para preservar a área do StatusBar no IOS

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
 
 import Cesta from './src/telas/cesta';
 
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
