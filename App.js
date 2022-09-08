94998809e24e92d6b5cd52e81d68445f923ebd36
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-09-08 05:27:28 -0300

    importação da fonte do Google Fonts pela biblioteca do EXPO
    importação da fonte do Google Fonts pela biblioteca do EXPO

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 
 import Cesta from './src/telas/cesta';
 
 export default function App() {
+  const [fonteCarregada] = useFonts ({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -13,7 +14,10 @@ export default function Cesta() {
 
     <View style={estilos.cesta}>
         <Text style={estilos.nome}>Cesta de Verduras</Text>
+        <View style={estilos.fazenda}>
+        <Image source={ logo } style={estilos.imagemFazenda} />
         <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        </View>
         <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
         <Text style={estilos.preco}>R$ 40,00</Text>
     </View>
@@ -44,11 +48,21 @@ const estilos = StyleSheet.create ({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontWeight: "bold"
+        fontFamily: "MontserratBold",
+    },
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32,
     },
     nomeFazenda: {
         fontSize: 16,
         lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: "MontserratRegular",
     },
     descricao: {
         color: "#A3A3A3",
