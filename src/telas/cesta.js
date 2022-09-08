77824d0f4e089b84876ed3a959370deae4e4250d
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-09-08 03:41:18 -0300

    Criação e estilização de uma View e quatro Text.
    Criação e estilização de uma View e quatro Text.

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, Text } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../assets/topo.png';
 
@@ -10,6 +10,14 @@ export default function Cesta() {
     return <>
     <Image source={ topo } style={estilos.topo} />
     <Text style={estilos.titulo}>Detalhe da cesta</Text>
+
+    <View style={estilos.cesta}>
+        <Text style={estilos.nome}>Cesta de Verduras</Text>
+        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+        <Text style={estilos.preco}>R$ 40,00</Text>
+    </View>
+
     </>
 }
 
@@ -27,5 +35,31 @@ const estilos = StyleSheet.create ({
         lineHeight: 26,
         color: "white",
         padding: 16
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nome: {
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold"
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 })
