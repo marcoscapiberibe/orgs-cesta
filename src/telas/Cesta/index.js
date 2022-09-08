26baa788aa4c8d3762e7c602691e970021832b4f
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-09-08 15:51:58 -0300

    Mudança de nome do arquivo Cesta.js para index.js. Criação do Componente Topo
    Mudança de nome do arquivo Cesta.js para index.js. Criação do Componente Topo

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
-import Texto from '../componentes/Texto';
+import Texto from '../../componentes/Texto';
 
-import topo from '../../assets/topo.png';
-import logo from '../../assets/logo.png';
+import logo from '../../../assets/logo.png';
+import Topo from './componentes/Topo';
 
-const width = Dimensions.get('screen').width;
 
 
 export default function Cesta() {
     return <>
-    <Image source={ topo } style={estilos.topo} />
-    <Texto style={estilos.titulo}>Detalhe da cesta</Texto>
-
+    <Topo />
     <View style={estilos.cesta}>
         <Texto style={estilos.nome}>Cesta de Verduras</Texto>
         <View style={estilos.fazenda}>
@@ -28,20 +25,6 @@ export default function Cesta() {
 }
 
 const estilos = StyleSheet.create ({
-    topo: {
-        width: "100%",
-        height: 578 / 768 * width,
-    },
-    titulo: {
-        width: "100%",
-        position: "absolute",
-        textAlign: "center",
-        fontSize: 16,
-        fontWeight: "bold",
-        lineHeight: 26,
-        color: "white",
-        padding: 16
-    },
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16,
